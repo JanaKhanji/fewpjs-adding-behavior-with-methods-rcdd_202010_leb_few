83fe4da4b6f919a22b50de4df97dee6c8a448abd
Jana Khanji <jana.khanji@ieee.org>
2020-12-13 10:07:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,6 +29,11 @@ class Bird {
   }
 
   speak() {
-    return this.name+"says squawk!"
+    if (this.sex == "male") {
+      return `It's me! ${this.name}, the parrot!`
+    } else {
+      return `${this.name} says squawk!`
+    }
+   
   }
 }
